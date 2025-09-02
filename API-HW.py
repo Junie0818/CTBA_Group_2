7d1d00c7e9a11096860e1041a6b98f2593467c8a
rmcole2488 <rmcole@wm.edu>
2025-09-02 12:29:00 -0400

    Update API-HW.py

--- API-HW.py
+++ API-HW.py
@@ -1,3 +1,6 @@
+# Team 2: Rachel Cole, Cade Haskins, Joshua Vasquez, Violet Zhao
+# We are using yfinance to pull Nvidia stock and compare it to the S&P 500 stock 
+
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +9,8 @@ import seaborn as sns
 NVDA = yf.Ticker("NVDA")
 # print(NVDA.history(period="1y"))
 
+#We used a period of 2 years to examine stocks from both Nvidia and S&P 500
+# We want to compare both stock sets and view Nvidia stocks reletaive to S&P
 NVDA_data = yf.download("NVDA", period="2y", auto_adjust=True)
 SP_data   = yf.download("^GSPC", period="2y", auto_adjust=True)
 
@@ -15,6 +20,7 @@ NVDA_copy = NVDA_data.copy().astype(float)
 for i in range(len(NVDA_copy.columns)):
     NVDA_copy.iloc[:, i] = (NVDA_copy.iloc[:, i] / SP_data.iloc[:, i] * 100).astype(float)
   
+# We are using the mean stocks for each month in the sampling period, originally we used everyday, but it gave a messy and overloaded graph 
 
 # recent_data.to_csv("NVDA.csv", index=False)
 # print(recent_data)
